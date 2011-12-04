92b8a287ccd8e475aaffe9090b61d4f364042ca8
Ted Dziuba <tjdziuba@gmail.com>
2011-12-03 21:59:25 -0800

    First dose of Chemo

---
+++ bin/adapters/_campfire/util.py
@@ -0,0 +1,19 @@
+import os
+import pyfire
+
+def get_campfire_room():
+    subdomain = os.environ.get("HUBOT_CAMPFIRE_SUBDOMAIN", None)
+    username = os.environ.get("HUBOT_CAMPFIRE_USERNAME", None)
+    password = os.environ.get("HUBOT_CAMPFIRE_PASSWORD", None)
+    room_name = os.environ.get("HUBOT_CAMPFIRE_ROOM", None)
+
+    if not all([subdomain, username, password, room_name]):
+        print ("You must set HUBOT_CAMPFIRE_SUBDOMAIN "
+               "HUBOT_CAMPFIRE_USERNAME, HUBOT_CAMPFIRE_PASSWORD "
+               "and HUBOT_CAMPFIRE_ROOM")
+        sys.exit(1)
+
+    campfire = pyfire.Campfire(subdomain, username, password, ssl=True)
+    room = campfire.get_room_by_name(room_name)
+    room.join()
+    return room
